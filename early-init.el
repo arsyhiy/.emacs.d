c34b1b976f5665cf6c2bfba88b703c1d3be2398b
arsyhiy <arsyhiy32@gmail.com>
2025-10-26 11:27:40 +0300

    adding a function in early-init.el chatgpt generated not refactored

--- early-init.el
+++ early-init.el
@@ -16,3 +16,20 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+
+
+(defun load-env-from-zsh ()
+  "Импортирует переменные окружения из интерактивной Zsh-сессии."
+  (interactive)
+  (let* ((shell "/bin/zsh") ; или другой путь, если у тебя zsh в другом месте
+         (output (shell-command-to-string
+                  (format "%s -i -c 'printenv'" shell))))
+    (dolist (line (split-string output "\n" t))
+      (when (string-match "\\`\\([^=]+\\)=\\(.*\\)\\'" line)
+        (setenv (match-string 1 line) (match-string 2 line))))
+    (when-let ((path (getenv "PATH")))
+      (setq exec-path (split-string path path-separator)))
+    (message "Zsh окружение загружено. gopls найден: %s"
+             (executable-find "gopls"))))
+
+(load-env-from-zsh)
