2aa354f86960943b0dc5867c908279910be3f2f0
arsyhiy <arsyhiy32@gmail.com>
2026-03-26 19:29:39 +0300

    update themes.el

--- lisp/ui/themes.el
+++ lisp/ui/themes.el
@@ -1,21 +1,9 @@
-;; lisp-themes.el  ---  -*- coding: utf-8; lexical-binding: t -*-
+;;; themes.el  ---  -*- coding: utf-8; lexical-binding: t -*-
 
-;; Copyright (C) 2025
-;; Version: 1.0
-;; packages-Requires: ((emacs "30"))
-;; Keywords: no keywords
-
-;; Commentary:
-
-;; no comment
-
-;; Code
-
-;; packages
 
 (use-package ef-themes
   :straight t
   :config (load-theme 'ef-cyprus t))
 
 (provide 'themes)
-;; lisp-themes.el ends here
+;;; themes.el ends here
