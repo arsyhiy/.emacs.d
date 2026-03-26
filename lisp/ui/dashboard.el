6ab7b6f4e79d628e0e0ef23d0c3ec0eac83131d5
arsyhiy <arsyhiy32@gmail.com>
2026-03-26 19:15:07 +0300

    update dashboard.el

--- lisp/ui/dashboard.el
+++ lisp/ui/dashboard.el
@@ -1,25 +1,13 @@
-;; lisp-dashboard.el  ---  -*- coding: utf-8; lexical-binding: t -*-
-
-;; Copyright (C) 2025
-;; Version: 1.0
-;; packages-Requires: ((emacs "30"))
-;; Keywords: no keywords
-
-;; Commentary:
-
-;; no comment
-
-;; Code 
-
-;; packages
+;;; dashboard.el  -*- coding: utf-8; lexical-binding: t; -*-
 
 (use-package dashboard
   :straight t
   :ensure t
-  :defer t 
   :demand t
   :init
-  (add-hook 'dashboard-mode-hook (lambda () (setq show-trailing-whitespace nil)))
+
+  (dashboard-setup-startup-hook)
+  :hook (dashboard-mode . (lambda () (setq show-trailing-whitespace nil)))
   :custom
   (dashboard-center-content t)
   (dashboard-set-heading-icons t)
@@ -28,35 +16,26 @@
   (dashboard-item-names '(("Recent Files:"               . "Recent files:")
                           ("Agenda for today:"           . "Today's agenda:")
                           ("Agenda for the coming week:" . "Agenda:")))
+
   (dashboard-item-shortcuts '((recents   . "r")
                               (bookmarks . "m")
    			      (projects  . "p")
    			      (agenda    . "a")
-    			      (registers . "e"))
-			    )
+    			      (registers . "e")))
 
   (dashboard-startupify-list '(dashboard-insert-banner
                                dashboard-insert-newline
-                               ;;dashboard-insert-banner-title
-                               ;;dashboard-insert-newline
                                dashboard-insert-navigator
                                dashboard-insert-newline
                                dashboard-insert-init-info
-                               dashboard-insert-items
-                               ;;dashboard-insert-newline
-                               ;;dashboard-insert-footer
-			       ))
+                               dashboard-insert-items))
 
-  (dashboard-display-icons-p t)     ;; display icons on both GUI and terminal
-  (dashboard-icon-type 'nerd-icons) ;; use `nerd-icons' package
+  (dashboard-display-icons-p t)
+  (dashboard-icon-type 'nerd-icons)
   (dashboard-icon-file-height 1.75)
   (dashboard-icon-file-v-adjust -0.125)
   (dashboard-heading-icon-height 1.75)
-  (dashboard-heading-icon-v-adjust -0.125)
+  (dashboard-heading-icon-v-adjust -0.125))
 
-  :config
-  (dashboard-setup-startup-hook))
-
-(require 'dashboard)
 (provide 'dashboard)
-;; lisp-dashboard.el ends here
+;; dashboard.el ends here
