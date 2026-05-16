9cfddd64f055c61af5b2ffda1152ecfc817694e2
arsyhiy <arsyhiy32@gmail.com>
2026-05-16 09:09:34 +0300

    update cc-arsyhiy.el

--- lisp/mode/cc-arsyhiy.el
+++ lisp/mode/cc-arsyhiy.el
@@ -7,15 +7,17 @@
 ;;; Code:
 (use-package cc-mode
   :ensure nil
-  :mode (("\\.c\\'"  . c-ts-mode)
-         ("\\.h\\'"  . c-ts-mode)
+  :mode (("\\.c\\'"   . c-ts-mode)
+         ("\\.h\\'"   . c-ts-mode)
          ("\\.cpp\\'" . c++-ts-mode)
          ("\\.hpp\\'" . c++-ts-mode))
   :hook ((c-ts-mode . (lambda ()
                         (setq indent-tabs-mode nil
-                              c-basic-offset 4)))
+                              c-basic-offset 4
+                              c-ts-mode-indent-offset 4)))
          (c++-ts-mode . (lambda ()
                           (setq indent-tabs-mode nil
-                                c-basic-offset 4)))))
+                                c-basic-offset 4
+                                c-ts-mode-indent-offset 4)))))
 (provide 'cc-arsyhiy)
 ;;; cc-arsyhiy.el ends here
